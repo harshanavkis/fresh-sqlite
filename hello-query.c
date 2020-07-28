fe6b4059651415cbac3e1c42ccbe78a3c682b7ca
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2020-07-28 23:05:28 +0100

    Performance counters: Measure overhead of (de)-encryption
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

--- hello-query.c
+++ hello-query.c
@@ -7,6 +7,7 @@
 #include "sqlite3.h"
 #include "mt_serialize.h"
 #include "mt_wrapper.h"
+#include "perf_counter.h"
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 {
@@ -29,6 +30,13 @@ int main(int argc, char const *argv[])
   char *zErrMsg = 0;
   int rc;
 
+  num_codec_enc = 0;
+  num_codec_dec = 0;
+
+  total_enc_time = 0;
+  total_kdf_time = 0;
+  mt_verify_time = 0;
+
 	mt_obj *tree = (mt_obj*) malloc(sizeof(mt_obj));
 	deserialize_init_mt(mt_path, tree);
 
@@ -43,13 +51,20 @@ int main(int argc, char const *argv[])
     return 1;
   }
 
-  rc = sqlite3_key(db, db_pwd, strlen(db_pwd), tree);
-  if(rc != SQLITE_OK)
+  if(strlen(db_pwd))
   {
-    fprintf(stderr, "Can't set key for database: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return 1;
+    rc = sqlite3_key(db, db_pwd, strlen(db_pwd), tree);
+    if(rc != SQLITE_OK)
+    {
+      fprintf(stderr, "Can't set key for database: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return 1;
+    }
   }
+
+  struct timeval tv1, tv2;
+
+  gettimeofday(&tv1, NULL);
 
   rc = sqlite3_exec(db, query, callback, 0, &zErrMsg);
   if(rc)
@@ -58,6 +73,18 @@ int main(int argc, char const *argv[])
     sqlite3_close(db);
     return 1;
   }
+
+  gettimeofday(&tv2, NULL);
+
+  printf ("Total query execution time = %f seconds\n",
+         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+         (double) (tv2.tv_sec - tv1.tv_sec));
+
+  printf("Total time spent in codec: %f seconds\n", total_enc_time);
+  printf("Total time for key derivation: %f seconds\n", total_kdf_time);
+  printf("Total time spent in merkle tree verification during decryption: %f seconds\n", mt_verify_time);
+  printf("Total number of encryptions: %u\n", num_codec_enc);
+  printf("Total number of decryptions: %u\n", num_codec_dec);
 
 	return 0;
 }
